66f6455d345bd4a8dce2da39a1c511b621c1b4e6
Mongezi Kgapola <mongezisamuel@gmail.com>
2024-08-23 10:24:18 +0200

    added : gameBoard logic, scoreCard object
    added methods for gameBoards logic
    - clearBoard
    - updateMove
    - showBoard
    
    added scoreCard object to keep score of rounds

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,46 @@
 // Board Object
 const gameBoard = (() => {
   console.log("Tic-Tac-Toe Board");
-  board = [];
+  let board = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+  ];
+
+  const clearBoard = () => {
+    board = [
+      [" ", " ", " "],
+      [" ", " ", " "],
+      [" ", " ", " "],
+    ];
+  };
+
+  const updateMove = (x, y, playerChar) => {
+    if (x >= 0 && x < 3 && y < 3 && y >= 0) {
+      board[x][y] = playerChar;
+    }
+  };
+
+  const showBoard = () => {
+    console.log(board[0]);
+    console.log(board[1]);
+    console.log(board[2]);
+  };
+
+  const getMoves = () => {
+    let moveList = [];
+    for (let i = 0; i < board[0].length; i++) {
+      for (let j = 0; j < board[(0, 0)].length; j++) {
+        const element = board[i][j];
+        if (element === "") {
+          moveList.push([i, j]);
+        }
+      }
+    }
+    return moveList;
+  };
+
+  return { clearBoard, updateMove, showBoard, getMoves };
 })();
 
 // Player General object factory function
@@ -22,11 +61,18 @@ const player = (playerName) => {
   return { getPlayerName, play, updateScore, getScore };
 };
 
-const gameFlow = (() => {
-  console.log("Game Flow");
+// Score card object
+
+const scoreCard = (() => {
+  console.log("Score Card");
+  let playerOneScore = 0;
+  let playerTwoScore = 0;
 })();
 
-gameBoard;
-gameFlow;
-const player1 = player("1");
-const player2 = player("2");
+const gameFlow = (() => {
+  const player1 = player("1");
+  const player2 = player("2");
+  console.log("Game Flow");
+
+  const startGame = () => {};
+})();
